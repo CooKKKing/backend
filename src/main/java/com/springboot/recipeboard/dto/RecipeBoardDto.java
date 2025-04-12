9fa8526b27fd1b88779113882c590a19c0cfd4b7
택현 <tjsk2222@gmail.com>
2025-04-12 19:53:02 +0900

    RecipeBoard Dto 일부 수정

--- src/main/java/com/springboot/recipeboard/dto/RecipeBoardDto.java
+++ src/main/java/com/springboot/recipeboard/dto/RecipeBoardDto.java
@@ -30,7 +30,7 @@ public class RecipeBoardDto {
         @Size(min = 1, max = 20, message = "메뉴 이름은 1자 이상 20자 이내여야 합니다.")
         @Pattern(
                 regexp = "^(?!\\s)(?!.*\\s{2,}).*$",
-                message = "제목은 공백으로 시작하거나 연속된 공백이 포함될 수 없습니다."
+                message = "메뉴이름은 공백으로 시작하거나 연속된 공백이 포함될 수 없습니다."
         )
         private String menuName;
 
