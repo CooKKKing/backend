b500274dc373fbaa5db86a25a4459ec63f8da3d0
택현 <tjsk2222@gmail.com>
2025-04-15 18:44:09 +0900

    MemberRepository 구현 (추후 코드 추가 및 수정 필요할 수 있음)

--- src/main/java/com/springboot/member/repository/MemberRepository.java
+++ src/main/java/com/springboot/member/repository/MemberRepository.java
@@ -1,6 +1,8 @@
 package com.springboot.member.repository;
 
 import com.springboot.member.entity.Member;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -8,4 +10,10 @@ import java.util.Optional;
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByEmail(String email);
     Optional<Member> findByLoginId(String loginId);
+    Optional<Member> findByPhoneNumber(String phoneNumber);
+    Optional<Member> findByNickNameAndPhoneNumber(String nickName, String phoneNumber);
+    Optional<Member> findByNickName(String nickName);
+    Page<Member> findByMemberStatus(Member.MemberStatus status, Pageable pageable);
+    Optional<Member> findByEmailAndPhoneNumber(String email, String phone);
+    Optional<Member> findByLoginIdAndEmail(String loginId, String email);
 }
