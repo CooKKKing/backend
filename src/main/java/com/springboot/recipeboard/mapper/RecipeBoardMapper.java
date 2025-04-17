49b16e91ace6cb545182cda792667aaab64f9422
택현 <tjsk2222@gmail.com>
2025-04-17 16:40:49 +0900

    내 레시피 게시글 조회를 위한 Mapper 추가

--- src/main/java/com/springboot/recipeboard/mapper/RecipeBoardMapper.java
+++ src/main/java/com/springboot/recipeboard/mapper/RecipeBoardMapper.java
@@ -1,10 +1,12 @@
 package com.springboot.recipeboard.mapper;
 
+import com.springboot.member.dto.MyRecipeBoardResponse;
 import com.springboot.recipeboard.dto.RecipeBoardDto;
 import com.springboot.recipeboard.entity.RecipeBoard;
 import org.mapstruct.Mapper;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Mapper(componentModel = "spring")
 public interface RecipeBoardMapper {
@@ -12,4 +14,19 @@ public interface RecipeBoardMapper {
     RecipeBoard recipeBoardPatchDtoToRecipeBoard(RecipeBoardDto.Patch recipeBoardPatchDto);
     List<RecipeBoardDto.Response> recipeBoardsToRecipeBoardResponseDtos(List<RecipeBoard> recipeBoards);
     RecipeBoardDto.Response recipeBoardToRecipeBoardResponseDto(RecipeBoard recipeBoard);
+    default MyRecipeBoardResponse recipeBoardToMyRecipeBoardResponse(RecipeBoard recipeBoard) {
+        return MyRecipeBoardResponse.builder()
+                .recipeBoardId(recipeBoard.getRecipeBoardId())
+                .title(recipeBoard.getTitle())
+                .menuName(recipeBoard.getMenuName())
+                .image(recipeBoard.getImage())
+                .likeCount(recipeBoard.getLikeCount())
+                .ingredients(
+                        recipeBoard.getRecipeBoardIngredients().stream()
+                                .map(rbi -> rbi.getIngredient().getIngredientName())
+                                .distinct()
+                                .collect(Collectors.toList())
+                )
+                .build();
+    }
 }
