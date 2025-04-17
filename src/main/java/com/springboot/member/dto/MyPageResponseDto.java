af90c9330e42bc7210b0681b3b23b5ad607e9763
택현 <tjsk2222@gmail.com>
2025-04-17 16:37:42 +0900

    내 정보 조회를 위한 MyPageResponseDto 구현

--- src/main/java/com/springboot/member/dto/MyPageResponseDto.java
+++ src/main/java/com/springboot/member/dto/MyPageResponseDto.java
@@ -3,11 +3,13 @@ package com.springboot.member.dto;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.util.List;
+
 @Getter
 @Builder
 public class MyPageResponseDto {
-    private String profileImage;
+    private String profile;
     private String nickName;
     private String title;
-    private int point;
+    private int ricePoint;
 }
