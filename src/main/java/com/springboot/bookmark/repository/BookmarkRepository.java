36abfd27ad48efa1b4d62427ff2b537d0230601d
택현 <tjsk2222@gmail.com>
2025-04-17 16:39:31 +0900

    내 북마크 게시글을 위해 findByMemberAndRecipeBoardId 추가

--- src/main/java/com/springboot/bookmark/repository/BookmarkRepository.java
+++ src/main/java/com/springboot/bookmark/repository/BookmarkRepository.java
@@ -7,6 +7,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
     Page<RecipeBoard> findRecipeBoardsByMember(Member member, Pageable pageable);
+    Optional<Bookmark> findByMemberAndRecipeBoardId(Member member, Long recipeBoardId);
 }
