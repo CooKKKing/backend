f9cb64f41f72e71cdbaabc66d235ac9b251c45a5
택현 <tjsk2222@gmail.com>
2025-04-20 03:46:49 +0900

    도감 Mapper 구현 완료

--- src/main/java/com/springboot/collection/mapper/CollectionMapper.java
+++ src/main/java/com/springboot/collection/mapper/CollectionMapper.java
@@ -1,17 +1,27 @@
 package com.springboot.collection.mapper;
 
 import com.springboot.collection.dto.CollectionDto;
+import com.springboot.collection.dto.CollectionItemDto;
 import com.springboot.collection.entity.Collection;
+import com.springboot.collection.entity.CollectionItem;
 import com.springboot.recipeboard.dto.RecipeBoardDto;
 import com.springboot.recipeboard.entity.RecipeBoard;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface CollectionMapper {
+    @Mapping(source = "categoryName", target = "customCategoryName")
     Collection collectionPostDtoToCollection(CollectionDto.Post collectionPostDto);
-    Collection collectionPatchDtoToCollection(CollectionDto.Patch collectionPatchDto);
+    @Mapping(source = "collectionId", target = "collectionId")
+    @Mapping(source = "collectionPatchDto.categoryName", target = "customCategoryName")
+    Collection collectionPatchDtoToCollection(long collectionId, CollectionDto.Patch collectionPatchDto);
     List<CollectionDto.Response> collectionsToCollectionResponseDtos(List<Collection> collections);
+    @Mapping(source = "customCategoryName", target = "categoryName")
     CollectionDto.Response collectionToCollectionResponseDto(Collection collection);
+    @Mapping(source = "image", target = "imageUrl")
+    CollectionItemDto.Response collectionItemToCollectionItemResponseDto(CollectionItem entity);
+    CollectionItem collectionItemPostDtoToCollectionItem(CollectionItemDto.Post dto);
 }
