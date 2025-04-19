db5d1d1db73e723dd46a93ab86a1369283ac2904
택현 <tjsk2222@gmail.com>
2025-04-20 03:47:13 +0900

    CollectionItem, Collection Repository 구현 완료

--- src/main/java/com/springboot/collection/repository/CollectionItemRepository.java
+++ src/main/java/com/springboot/collection/repository/CollectionItemRepository.java
@@ -1,7 +1,12 @@
 package com.springboot.collection.repository;
 
+import com.springboot.collection.dto.CollectionItemDto;
+import com.springboot.collection.entity.Collection;
 import com.springboot.collection.entity.CollectionItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CollectionItemRepository extends JpaRepository<CollectionItem, Long> {
+    List<CollectionItem> findByCollection(Collection collection);
 }
--- src/main/java/com/springboot/collection/repository/CollectionRepository.java
+++ src/main/java/com/springboot/collection/repository/CollectionRepository.java
@@ -1,7 +1,14 @@
 package com.springboot.collection.repository;
 
 import com.springboot.collection.entity.Collection;
+import com.springboot.collection.entity.CollectionItem;
+import com.springboot.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CollectionRepository extends JpaRepository<Collection, Long> {
+    boolean existsByMemberAndCustomCategoryName(Member member, String customCategoryName);
+    boolean existsByCustomCategoryNameAndMember_MemberId(String customCategoryName, long memberId);
+    List<Collection> findByMember(Member member);
 }
