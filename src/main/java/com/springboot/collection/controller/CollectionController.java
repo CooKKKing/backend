3a97b3ac5f3d23aec3d8eda244f3755446e7ba77
택현 <tjsk2222@gmail.com>
2025-04-20 03:44:42 +0900

    도감 Controller 구현 완료(메뉴 이미지 업로드만 구현 하면 됨)

--- src/main/java/com/springboot/collection/controller/CollectionController.java
+++ src/main/java/com/springboot/collection/controller/CollectionController.java
@@ -1,26 +1,50 @@
 package com.springboot.collection.controller;
 
 import com.springboot.collection.dto.CollectionDto;
+import com.springboot.collection.dto.CollectionItemDto;
+import com.springboot.collection.entity.Collection;
+import com.springboot.collection.entity.CollectionItem;
+import com.springboot.collection.mapper.CollectionMapper;
+import com.springboot.collection.service.CollectionService;
+import com.springboot.file.Service.FileSystemStorageService;
 import com.springboot.member.entity.Member;
+import com.springboot.member.mapper.MemberMapper;
+import com.springboot.member.service.MemberService;
+import com.springboot.utils.UriCreator;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
 import javax.validation.constraints.Positive;
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Tag(name = "도감 컨트롤러", description = "도감 관련 컨트롤러")
 @RestController
 @RequestMapping("/collections")
 @Validated
 public class CollectionController {
+    private static final String COLLECTION_DEFAULT_URL = "/collections";
+    private final CollectionService collectionService;
+    private final CollectionMapper mapper;
+
+    public CollectionController(CollectionService collectionService, CollectionMapper mapper) {
+        this.collectionService = collectionService;
+        this.mapper = mapper;
+    }
+
     @Operation(summary = "도감 카테고리 생성", description = "도감 카테고리를 생성합니다.")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "201", description = "도감 카테고리 생성 완료"),
@@ -29,9 +53,13 @@ public class CollectionController {
     @PostMapping
     public ResponseEntity postCollection(@Valid @RequestBody CollectionDto.Post collectionPostDto,
                                          @Parameter(hidden = true) @AuthenticationPrincipal Member member) {
-        // Post Controller 로직 작성 해야함
+        Collection collection = mapper.collectionPostDtoToCollection(collectionPostDto);
 
-        return new ResponseEntity<>(HttpStatus.CREATED);
+        Collection customCollection = collectionService.createCollection(collection, member.getMemberId());
+
+        URI location = UriCreator.createUri(COLLECTION_DEFAULT_URL, customCollection.getCollectionId());
+
+        return ResponseEntity.created(location).build();
     }
 
     @Operation(summary = "도감 카테고리 수정", description = "도감 카테고리를 수정합니다.")
@@ -43,7 +71,10 @@ public class CollectionController {
     public ResponseEntity patchCollection(@PathVariable("collection-id") @Positive long collectionId,
                                           @Valid @RequestBody CollectionDto.Patch collectionPatchDto,
                                           @Parameter(hidden = true) @AuthenticationPrincipal Member member) {
-        // Patch Controller 로직 작성 해야함
+
+        Collection collection = mapper.collectionPatchDtoToCollection(collectionId, collectionPatchDto);
+
+        collectionService.updateCollection(collection, member.getMemberId());
 
         return new ResponseEntity<>(HttpStatus.OK);
     }
@@ -57,7 +88,12 @@ public class CollectionController {
     public ResponseEntity getCollection(@Parameter(hidden = true) @AuthenticationPrincipal Member member) {
         // 도감 카테고리 전체 조회 로직 작성 해야함
 
-        return new ResponseEntity<>(HttpStatus.OK);
+        List<Collection> collections = collectionService.findCollections(member.getMemberId());
+        List<CollectionDto.Response> response = collections.stream()
+                .map(mapper::collectionToCollectionResponseDto)
+                .collect(Collectors.toList());
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     @Operation(summary = "도감 카테고리 삭제", description = "도감 카테고리를 삭제합니다.")
@@ -66,24 +102,27 @@ public class CollectionController {
             @ApiResponse(responseCode = "400", description = "Collection Validation failed")
     })
     @DeleteMapping("/{collection-id}")
-    public ResponseEntity deleteCollection(@PathVariable @Positive long collectionId,
-                                           @AuthenticationPrincipal Member member) {
-        // Delete Controller 로직 작성 해야함
+    public ResponseEntity deleteCollection(@PathVariable("collection-id") @Positive long collectionId,
+                                           @Parameter(hidden = true) @AuthenticationPrincipal Member member) {
+        collectionService.deleteCollection(collectionId, member.getMemberId());
 
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
-    @Operation(summary = "도감 카테고리 내 메뉴 목록 조회", description = "도감 카테고리 내 메뉴 목록을 조회합니다.")
+    @Operation(summary = "도감 카테고리 내 메뉴 전체 조회", description = "도감 카테고리 내 메뉴들을 조회합니다.")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "도감 카테고리 내 메뉴 목록 조회 완료"),
+            @ApiResponse(responseCode = "200", description = "도감 카테고리 내 메뉴 전체 조회 완료"),
             @ApiResponse(responseCode = "400", description = "Collection Validation failed")
     })
     @GetMapping("/{collection-id}/collectionitem")
-    public ResponseEntity getCollectionItem(@PathVariable @Positive long collectionId,
-                                            @AuthenticationPrincipal Member member) {
-        // 도감 카테고리 내 메뉴 목록 조회 Controller 로직 작성 해야함
+    public ResponseEntity getCollectionItem(@PathVariable("collection-id") @Positive long collectionId,
+                                            @Parameter(hidden = true) @AuthenticationPrincipal Member member) {
+        List<CollectionItem> collectionItems = collectionService.findCollectionItems(collectionId, member.getMemberId());
+        List<CollectionItemDto.Response> response = collectionItems.stream()
+                .map(mapper::collectionItemToCollectionItemResponseDto)
+                .collect(Collectors.toList());
 
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     @Operation(summary = "도감 카테고리 메뉴 추가", description = "도감 카테고리 메뉴를 추가합니다.")
@@ -91,23 +130,37 @@ public class CollectionController {
             @ApiResponse(responseCode = "200", description = "도감 카테고리 메뉴 추가 완료"),
             @ApiResponse(responseCode = "400", description = "Collection Validation failed")
     })
-    @PostMapping("/{collection-id}/collectionitem")
-    public ResponseEntity postCollectionItem(@PathVariable @Positive long collectionId,
-                                             @AuthenticationPrincipal Member member) {
-        // 도감 카테고리 메뉴 추가 Controller 로직 작성 해야함
+    @PostMapping(value = "/{collection-id}/collectionitem", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity postCollectionItem(
+            @PathVariable("collection-id") @Positive long collectionId,
+            @RequestPart @Valid CollectionItemDto.Post collectionItemPostDto,
+            @RequestPart(value = "collectionItemImage", required = false) MultipartFile collectionItemImage,
+            @Parameter(hidden = true) @AuthenticationPrincipal Member member) {
 
-        return new ResponseEntity<>(HttpStatus.CREATED);
+        CollectionItem collectionItem = mapper.collectionItemPostDtoToCollectionItem(collectionItemPostDto);
+
+        CollectionItem postItem = collectionService.addCollectionItem(
+                collectionId,
+                collectionItem,
+                collectionItemImage,
+                member.getMemberId()
+        );
+
+        CollectionItemDto.Response response = mapper.collectionItemToCollectionItemResponseDto(postItem);
+
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
+
 
     @Operation(summary = "도감 카테고리 메뉴 삭제", description = "도감 카테고리 메뉴를 삭제합니다.")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "도감 카테고리 메뉴 삭제 완료"),
+            @ApiResponse(responseCode = "204", description = "도감 카테고리 메뉴 삭제 완료"),
             @ApiResponse(responseCode = "400", description = "Collection Validation failed")
     })
     @DeleteMapping("/collectionitems/{collectionitem-id}")
-    public ResponseEntity deleteCollectionItem(@PathVariable @Positive long collectionItemId,
-                                               @AuthenticationPrincipal Member member) {
-        // 도감 카테고리 메뉴 삭제 Controller 로직 작성 해야함
+    public ResponseEntity deleteCollectionItem(@PathVariable("collectionitem-id") @Positive long collectionItemId,
+                                               @Parameter(hidden = true) @AuthenticationPrincipal Member member) {
+        collectionService.deleteCollectionItem(collectionItemId, member.getMemberId());
 
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
