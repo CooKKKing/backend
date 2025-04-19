5403ed9aaa6b6b2a39ca06a0351c15692925c412
택현 <tjsk2222@gmail.com>
2025-04-20 03:45:18 +0900

    CollectionDto 일부 수정 및 구현 완료

--- src/main/java/com/springboot/collection/dto/CollectionDto.java
+++ src/main/java/com/springboot/collection/dto/CollectionDto.java
@@ -1,6 +1,7 @@
 package com.springboot.collection.dto;
 
 
+import com.springboot.audit.BaseEntity;
 import com.springboot.collection.entity.Collection;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
@@ -17,15 +18,6 @@ import java.util.List;
 public class CollectionDto {
     @Getter
     public static class Post {
-        @Schema(description = "도감 내 메뉴 이름", example = "다 태워먹은 감자전")
-        @NotBlank(message = "메뉴 이름은 필수입니다.")
-        @Size(min = 1, max = 20, message = "메뉴 이름은 1자 이상 20자 이내여야 합니다.")
-        @Pattern(
-                regexp = "^(?!\\s)(?!.*\\s{2,}).*$",
-                message = "메뉴이름은 공백으로 시작하거나 연속된 공백이 포함될 수 없습니다."
-        )
-        private String menuName;
-
         @Schema(description = "도감 카테고리 이름", example = "실패해서 다시 도전할 음식")
         @NotBlank(message = "도감 카테고리 이름은 필수입니다.")
         @Size(min = 1, max = 20, message = "카테고리 이름은 1자 이상 20자 이내여야 합니다.")
@@ -34,10 +26,6 @@ public class CollectionDto {
                 message = "카테고리 이름은 공백으로 시작하거나 연속된 공백이 포함될 수 없습니다."
         )
         private String categoryName;
-
-        @Schema(description = "도감 메뉴 대표 이미지 URL", example = "https://img.cookking.com/menu1.jpg")
-        @NotBlank(message = "이미지 URL은 필수입니다.")
-        private String imageUrl;
     }
 
     @Getter
@@ -51,10 +39,6 @@ public class CollectionDto {
                 message = "카테고리 이름은 공백으로 시작하거나 연속된 공백이 포함될 수 없습니다."
         )
         private String categoryName;
-
-        @Schema(description = "도감 메뉴 대표 이미지 URL", example = "https://img.cookking.com/menu1.jpg")
-        @NotBlank(message = "이미지 URL은 필수입니다.")
-        private String imageUrl;
     }
 
     @Getter
@@ -68,15 +52,5 @@ public class CollectionDto {
 
         @Schema(description = "도감 공개 상태", example = "PUBLIC")
         private Collection.CollectionStatus collectionStatus;
-
-        @Schema(description = "도감 생성일자", example = "2025-04-10T15:00:00")
-        private LocalDateTime createdAt;
-
-        @Schema(description = "도감에 등록된 메뉴 개수", example = "3")
-        private int menuCount;
-
-        @Schema(description = "도감에 등록된 메뉴 이미지 목록 (사용자가 직접 업로드한 이미지)",
-                example = "[\"https://cookking.com/uploads/user1_menu1.jpg\", \"https://cookking.com/uploads/user1_menu2.jpg\"]")
-        private List<String> userUploadedImages;
     }
 }
