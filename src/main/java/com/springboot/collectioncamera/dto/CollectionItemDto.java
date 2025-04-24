96a6ea1dc30d26ce8d855593091076c2abe13901
택현 <tjsk2222@gmail.com>
2025-04-24 16:21:38 +0900

    CollectionItemDto 구현

--- src/main/java/com/springboot/collectioncamera/dto/CollectionItemDto.java
+++ src/main/java/com/springboot/collectioncamera/dto/CollectionItemDto.java
@@ -1,4 +1,4 @@
-package com.springboot.collection.dto;
+package com.springboot.collectioncamera.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
@@ -13,17 +13,17 @@ public class CollectionItemDto {
         @NotBlank(message = "메뉴 이름은 필수입니다.")
         @Schema(description = "도감에 등록할 음식 이름", example = "공기밥")
         private String menuName;
+
+        @NotBlank(message = "이미지 URL은 필수입니다.")
+        @Schema(description = "도감에 등록할 음식 이미지 URL", example = "https://s3.bucket/collections/item_123.png")
+        private String imageUrl;
     }
 
     @Getter
     @Builder
     public static class Response {
         private long collectionItemId;
-
-        @Schema(description = "도감에 등록된 음식 이름", example = "카레덮밥")
         private String menuName;
-
-        @Schema(description = "도감에 등록된 음식 이미지 URL", example = "https://img.cookking.com/menus/curry.jpg")
         private String imageUrl;
     }
 }
