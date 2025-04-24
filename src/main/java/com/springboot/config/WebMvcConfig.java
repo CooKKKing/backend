156140fc983d025b3a0fc01ec3a954edec0c33ab
택현 <tjsk2222@gmail.com>
2025-04-24 18:01:36 +0900

    사용 안해서 주석 처리

--- src/main/java/com/springboot/config/WebMvcConfig.java
+++ src/main/java/com/springboot/config/WebMvcConfig.java
@@ -7,22 +7,22 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry
-                .addResourceHandler("/images/**")  // URL 경로
-                //file 프로토콜(로컬)
-                .addResourceLocations("file:" + System.getProperty("user.dir") + "/fileImage/");
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry
+//                .addResourceHandler("/images/**")  // URL 경로
+//                //file 프로토콜(로컬)
+//                .addResourceLocations("file:" + System.getProperty("user.dir") + "/fileImage/");
 //                .addResourceLocations( "classpath:/static/images/",
 //                        "file:/app/fileImage/");
-    }
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
-                .allowedMethods("*")
-                .allowedHeaders("*")
-                .allowCredentials(true);
-    }
+//    }
+//
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOrigins("http://localhost:3000")
+//                .allowedMethods("*")
+//                .allowedHeaders("*")
+//                .allowCredentials(true);
+//    }
 }
