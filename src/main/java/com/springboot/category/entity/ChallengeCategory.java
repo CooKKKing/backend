3bc9143e85ea5df9b84f7530f1b12e5f441840d1
택현 <tjsk2222@gmail.com>
2025-04-24 16:19:22 +0900

    ChallengeCategory Entity 수정

--- src/main/java/com/springboot/category/entity/ChallengeCategory.java
+++ src/main/java/com/springboot/category/entity/ChallengeCategory.java
@@ -1,5 +1,6 @@
 package com.springboot.category.entity;
 
+import com.springboot.audit.BaseEntity;
 import com.springboot.challenge.entity.Challenge;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,7 +14,7 @@ import java.util.List;
 @Setter
 @NoArgsConstructor
 @Entity
-public class ChallengeCategory {
+public class ChallengeCategory extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long challengeCategoryId;
