978dacd3ba54304ba0c03ad8a8d7fed123ef79ba
택현 <tjsk2222@gmail.com>
2025-04-24 16:23:31 +0900

    CollectionCameraMapper 구현

--- src/main/java/com/springboot/collectioncamera/mapper/CollectionCameraMapper.java
+++ src/main/java/com/springboot/collectioncamera/mapper/CollectionCameraMapper.java
@@ -1,7 +1,8 @@
 package com.springboot.collectioncamera.mapper;
 
-import com.springboot.collectioncamera.dto.CollectionDto;
+import com.springboot.collectioncamera.dto.CollectionCameraDto;
 import com.springboot.collectioncamera.dto.CollectionItemDto;
+import com.springboot.collectioncamera.entity.CameraImage;
 import com.springboot.collectioncamera.entity.CollectionCamera;
 import com.springboot.collectioncamera.entity.CollectionItem;
 import org.mapstruct.Mapper;
@@ -10,16 +11,21 @@ import org.mapstruct.Mapping;
 import java.util.List;
 
 @Mapper(componentModel = "spring")
-public interface CollectionMapper {
-    @Mapping(source = "categoryName", target = "customCategoryName")
-    CollectionCamera collectionPostDtoToCollection(CollectionDto.Post collectionPostDto);
-    @Mapping(source = "collectionId", target = "collectionId")
-    @Mapping(source = "collectionPatchDto.categoryName", target = "customCategoryName")
-    CollectionCamera collectionPatchDtoToCollection(long collectionId, CollectionDto.Patch collectionPatchDto);
-    List<CollectionDto.Response> collectionsToCollectionResponseDtos(List<CollectionCamera> collectionCameras);
-    @Mapping(source = "customCategoryName", target = "categoryName")
-    CollectionDto.Response collectionToCollectionResponseDto(CollectionCamera collectionCamera);
-    @Mapping(source = "image", target = "imageUrl")
+public interface CollectionCameraMapper {
+    CollectionCamera collectionCameraPostDtoToCollectionCamera(CollectionCameraDto.Post Dto);
+    List<CollectionCameraDto.Response> collectionsToCollectionResponseDtos(List<CollectionCamera> collectionCameras);
+    @Mapping(target = "imageUrl", expression = "java(mapCameraImage(collectionCamera))")
+    CollectionCameraDto.Response collectionCameraToCollectionCameraResponseDto(CollectionCamera collectionCamera);
     CollectionItemDto.Response collectionItemToCollectionItemResponseDto(CollectionItem entity);
     CollectionItem collectionItemPostDtoToCollectionItem(CollectionItemDto.Post dto);
+    List<CollectionCameraDto.ResponseImage> cameraImagesToCameraImageResponseDtos(List<CameraImage> cameraImages);
+
+
+    default String mapCameraImage(CollectionCamera collectionCamera) {
+        if (collectionCamera.getCameraImage() != null) {
+            return collectionCamera.getCameraImage().getImageUrl();
+        } else {
+            return null; // 이미지 없으면 null
+        }
+    }
 }
