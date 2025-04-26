10488c1dea21587e8c344d7331cf82641fd433e2
택현 <tjsk2222@gmail.com>
2025-04-26 22:29:47 +0900

    category package 삭제

--- src/main/java/com/springboot/category/repository/ChallengeCategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.springboot.category.repository;
-
-import com.springboot.challenge.entity.Challenge;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ChallengeCategoryRepository extends JpaRepository<Challenge, Long> {
-}
