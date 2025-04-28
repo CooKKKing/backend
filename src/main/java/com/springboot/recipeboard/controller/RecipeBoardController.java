c356e11f30c6abdd6256645d99a98a398548f7ee
택현 <tjsk2222@gmail.com>
2025-04-28 23:38:40 +0900

    RecipeBoard 전체 조회 비회원도 볼 수 있게 fix

--- src/main/java/com/springboot/recipeboard/controller/RecipeBoardController.java
+++ src/main/java/com/springboot/recipeboard/controller/RecipeBoardController.java
@@ -65,13 +65,18 @@ public class RecipeBoardController {
     public ResponseEntity patchRecipeBoard(@PathVariable("recipe-id") @Positive long recipeId,
                                            @Valid @RequestBody RecipeBoardDto.Patch recipeBoardPatchDto,
                                            @Parameter(hidden = true) @AuthenticationPrincipal Member member) {
+        // Patch Controller 로직 작성 해야함
+        member = new Member(); // 임시로 Member 객체 생성
+        member.setMemberId(1L); // 임시로 memberId 설정
+
+        long memberId = member.getMemberId();
 
         recipeBoardPatchDto.setRecipeBoardId(recipeId);
 
         RecipeBoard recipeBoard = mapper.recipeBoardPatchDtoToRecipeBoard(recipeBoardPatchDto);
-        RecipeBoard updatedRecipeBoard = recipeBoardService.updateRecipeBoard(recipeBoard, member.getMemberId());
+        RecipeBoard updatedRecipeBoard = recipeBoardService.updateRecipeBoard(recipeBoard, memberId);
 
-        return new ResponseEntity<>(new SingleResponseDto<>(mapper.recipeBoardToRecipeBoardResponseDto(updatedRecipeBoard,member.getMemberId())), HttpStatus.OK);
+        return new ResponseEntity<>(new SingleResponseDto<>(mapper.recipeBoardToRecipeBoardResponseDto(updatedRecipeBoard,memberId)), HttpStatus.OK);
     }
 
     @Operation(summary = "레시피 게시글 단일 조회", description = "레시피 게시글을 단일 조회합니다.")
@@ -84,7 +89,10 @@ public class RecipeBoardController {
                                          @Parameter(hidden = true) @AuthenticationPrincipal Member member) {
         // Get Controller 로직 작성 해야함
         RecipeBoard recipeBoard = recipeBoardService.findRecipeBoard(recipeId);
-        RecipeBoardDto.Response response = mapper.recipeBoardToRecipeBoardResponseDto(recipeBoard, member.getMemberId());
+
+        RecipeBoardDto.Response response = member == null
+                ? mapper.recipeBoardToRecipeBoardResponseDto(recipeBoard)
+                : mapper.recipeBoardToRecipeBoardResponseDto(recipeBoard, member.getMemberId());
 
         return new ResponseEntity<>(new SingleResponseDto<>(response), HttpStatus.OK);
     }
@@ -103,24 +111,6 @@ public class RecipeBoardController {
         return ResponseEntity.noContent().build();
     }
 
-    @Operation(summary = "레시피 게시글 전체 조회", description = "레시피 게시글을 전체 조회합니다.")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "레시피 게시글 전체 조회 완료"),
-            @ApiResponse(responseCode = "400", description = "RecipeBoard Validation failed")
-    })
-    @GetMapping
-    public ResponseEntity getRecipeBoards(@Parameter(hidden = true)@AuthenticationPrincipal Member member,
-                                          @Positive @RequestParam(defaultValue = "1") int page,
-                                          @Positive @RequestParam(defaultValue = "50") int size) {
-        // 레시피 게시글 전체 조회 Controller 로직 작성 해야함
-        Page<RecipeBoard> pageBoards = recipeBoardService.findAllRecipeBoards(page - 1, size);
-        List<RecipeBoard> recipeBoards = pageBoards.getContent();
-        // RecipeBoard 전체 조회 후, RecipeBoardResponseDto로 변환
-
-        return new ResponseEntity(new MultiResponseDto<>(mapper.recipeBoardsToRecipeBoardResponseDtos(recipeBoards, member.getMemberId()),
-                pageBoards), HttpStatus.OK);
-    }
-
     @Operation(summary = "카테고리별 레시피 게시글 전체 조회", description = "카테고리별 레시피 게시글을 전체 조회합니다.")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "카테고리별 레시피 게시글 전체 조회 완료"),
@@ -135,7 +125,32 @@ public class RecipeBoardController {
         Page<RecipeBoard> pageBoards = recipeBoardService.findCategoryRecipeBoards(page - 1, size, categoryId);
         List<RecipeBoard> recipeBoards = pageBoards.getContent();
 
-        return new ResponseEntity(new MultiResponseDto<>(mapper.recipeBoardsToRecipeBoardResponseDtos(recipeBoards, member.getMemberId()),
+        List<RecipeBoardDto.Response> responses =
+                member == null
+                        ? mapper.recipeBoardsToRecipeBoardResponseDtos(recipeBoards)
+                        : mapper.recipeBoardsToRecipeBoardResponseDtos(recipeBoards, member.getMemberId());
+        return new ResponseEntity(new MultiResponseDto<>(responses,
+                pageBoards), HttpStatus.OK);
+    }
+
+    @Operation(summary = "레시피 게시글 전체 조회", description = "레시피 게시글을 전체 조회합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "레시피 게시글 전체 조회 완료"),
+            @ApiResponse(responseCode = "400", description = "RecipeBoard Validation failed")
+    })
+    @GetMapping
+    public ResponseEntity getRecipeBoards(@Parameter(hidden = true)@AuthenticationPrincipal Member member,
+                                          @Positive @RequestParam(defaultValue = "1") int page,
+                                          @Positive @RequestParam(defaultValue = "50") int size) {
+        // 레시피 게시글 전체 조회 Controller 로직 작성 해야함
+        Page<RecipeBoard> pageBoards = recipeBoardService.findAllRecipeBoards(page - 1, size);
+        List<RecipeBoard> recipeBoards = pageBoards.getContent();
+        // RecipeBoard 전체 조회 후, RecipeBoardResponseDto로 변환
+        List<RecipeBoardDto.Response> responses =
+                member == null
+                        ? mapper.recipeBoardsToRecipeBoardResponseDtos(recipeBoards)
+                        : mapper.recipeBoardsToRecipeBoardResponseDtos(recipeBoards, member.getMemberId());
+        return new ResponseEntity(new MultiResponseDto<>(responses,
                 pageBoards), HttpStatus.OK);
     }
 
@@ -154,7 +169,11 @@ public class RecipeBoardController {
         Page<RecipeBoard> pageBoards = recipeBoardService.findMenuRecipeBoards(page - 1, size, menuId);
         List<RecipeBoard> recipeBoards = pageBoards.getContent();
 
-        return new ResponseEntity(new MultiResponseDto<>(mapper.recipeBoardsToRecipeBoardResponseDtos(recipeBoards, member.getMemberId()),
+        List<RecipeBoardDto.Response> responses =
+                member == null
+                        ? mapper.recipeBoardsToRecipeBoardResponseDtos(recipeBoards)
+                        : mapper.recipeBoardsToRecipeBoardResponseDtos(recipeBoards, member.getMemberId());
+        return new ResponseEntity(new MultiResponseDto<>(responses,
                 pageBoards), HttpStatus.OK);
     }
 
