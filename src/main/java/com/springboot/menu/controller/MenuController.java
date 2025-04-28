477e1e865080dcb8805efb4198a3d7c824d3702e
택현 <tjsk2222@gmail.com>
2025-04-28 23:37:18 +0900

    메뉴 추천 기능 구현

--- src/main/java/com/springboot/menu/controller/MenuController.java
+++ src/main/java/com/springboot/menu/controller/MenuController.java
@@ -2,17 +2,22 @@ package com.springboot.menu.controller;
 
 import com.springboot.dto.MultiResponseDto;
 import com.springboot.dto.SingleResponseDto;
+import com.springboot.ingredient.mapper.IngredientMapper;
 import com.springboot.member.entity.Member;
 import com.springboot.menu.dto.MenuDto;
 import com.springboot.menu.entity.Menu;
 import com.springboot.menu.mapper.MenuMapper;
 import com.springboot.menu.service.MenuService;
+import com.springboot.recipeboard.entity.RecipeBoard;
+import com.springboot.recipeboard.mapper.RecipeBoardMapper;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -27,34 +32,40 @@ import java.util.List;
 public class MenuController {
     private final MenuService menuService;
     private final MenuMapper menuMapper;
+    private final IngredientMapper ingredientMapper;
+    private final RecipeBoardMapper recipeBoardMapper;
     private static final String MENU_URI = "menus/";
 
-    public MenuController(MenuService menuService, MenuMapper menuMapper) {
+    public MenuController(MenuService menuService, MenuMapper menuMapper, IngredientMapper ingredientMapper, RecipeBoardMapper recipeBoardMapper) {
         this.menuService = menuService;
         this.menuMapper = menuMapper;
+        this.ingredientMapper = ingredientMapper;
+        this.recipeBoardMapper = recipeBoardMapper;
     }
 
-    @Operation(summary = "메뉴 추천", description = "사용자가 선택한 재료를 바탕으로 카테고리별 메뉴를 추천합니다.")
+    @Operation(summary = "메뉴 추천 및 재추천", description = "사용자가 선택한 재료를 바탕으로 카테고리별 메뉴를 추천합니다. 매번 랜덤하게 레시피를 추천합니다.")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "추천된 메뉴 조회 완료"),
             @ApiResponse(responseCode = "400", description = "잘못된 재료 선택 (주재료 2개 이상 필수)")
     })
-    @PostMapping("/recommendations/")
+    @PostMapping("/recommendations")
     public ResponseEntity recommendMenus( // RequestBody 담아야됨
-                                          @Parameter(hidden = true) @AuthenticationPrincipal Member member) {
+                                          @Parameter(hidden = true) @AuthenticationPrincipal Member member,
+                                          @RequestBody MenuDto.RecommendationsIngredientDto IngredientDto,
+                                          @Parameter(description = "페이지, 기본값은 1") @RequestParam(value = "page", defaultValue = "1") int page,
+                                          @Parameter(description = "사이즈, 기본값은 10") @RequestParam(value = "size", defaultValue = "10") int size) {
 
-        return new ResponseEntity<>(HttpStatus.OK);
-    }
+        List<RecipeBoard> recipeBoard = menuService.recommendMenus(ingredientMapper.ingredientsDtoToIngredients(IngredientDto), page - 1, size);
+//        List<RecipeBoard> recipeBoardList = recipeBoard.getContent();
+        Page<RecipeBoard> pageImpe = new PageImpl<>(
+                recipeBoard,                      // content
+                PageRequest.of(page - 1, size),     // pageable (현재 page-1 주는 거 맞게)
+                recipeBoard.size()                 // totalElements (전체 개수 그냥 리스트 사이즈로)
+        );
 
-    @Operation(summary = "메뉴 재추천", description = "추천 버튼을 눌러 다른 메뉴를 재추천 받습니다.")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "재추천된 메뉴 조회 완료")
-    })
-    @PostMapping("/recommendations/refresh")
-    public ResponseEntity refreshRecommendedMenus( // RequestBody 담아야됨
-                                                   @Parameter(hidden = true) @AuthenticationPrincipal Member member) {
-
-        return new ResponseEntity<>(HttpStatus.OK);
+        return new ResponseEntity(
+                new MultiResponseDto<>(recipeBoardMapper.recipeBoardsToRecipeBoardResponseDtos(recipeBoard),
+                        pageImpe), HttpStatus.OK);
     }
 
     @Operation(summary = "메뉴 등록", description = "새로운 메뉴를 등록합니다.")
