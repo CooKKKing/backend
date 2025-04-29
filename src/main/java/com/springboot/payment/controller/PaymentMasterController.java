16a2847c4e8163d34e81f9fabdcd7c7397e3c6d9
택현 <tjsk2222@gmail.com>
2025-04-29 20:22:29 +0900

    Payment Swagger fix

--- src/main/java/com/springboot/payment/controller/PaymentMasterController.java
+++ src/main/java/com/springboot/payment/controller/PaymentMasterController.java
@@ -3,6 +3,8 @@ package com.springboot.payment.controller;
 import com.springboot.payment.entity.PaymentMaster;
 import com.springboot.payment.service.PaymentMasterService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -19,6 +21,10 @@ public class PaymentMasterController {
     private final PaymentMasterService paymentMasterService;
 
     @Operation(summary = "밥풀 상품 전체 조회", description = "밥풀 상점에 등록된 모든 상품을 조회합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "밥풀 상점 조회 성공"),
+            @ApiResponse(responseCode = "401", description = "인증 실패"),
+    })
     @GetMapping
     public ResponseEntity getAllMasters() {
         List<PaymentMaster> masters = paymentMasterService.findAllMasters();
