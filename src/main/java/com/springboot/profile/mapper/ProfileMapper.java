a8f44a8203ee89ad0bd1245546402fcd042de2e7
택현 <tjsk2222@gmail.com>
2025-05-22 17:02:32 +0900

    ProfileMapper 메서드 추가

--- src/main/java/com/springboot/profile/mapper/ProfileMapper.java
+++ src/main/java/com/springboot/profile/mapper/ProfileMapper.java
@@ -7,6 +7,7 @@ import com.springboot.profile.entity.ProfileImage;
 import org.mapstruct.Mapper;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Mapper(componentModel = "spring")
 public interface ProfileMapper {
@@ -23,4 +24,18 @@ public interface ProfileMapper {
 
         return memberProfileImage;
     }
+
+    default List<ProfileDto.OwnershipResponse> profileImagesToOwnershipDtos(List<ProfileImage> profileImages, long memberId) {
+        return profileImages.stream().map(profileImage -> {
+            ProfileDto.OwnershipResponse response = new ProfileDto.OwnershipResponse();
+            response.setProfileId(profileImage.getProfileImageId());
+            response.setImagePath(profileImage.getImagePath());
+            response.setPrice(profileImage.getPrice());
+            boolean isOwned = profileImage.getMemberProfileImages().stream()
+                    .anyMatch(mpi -> mpi.getMember().getMemberId() == memberId);
+            response.setOwned(isOwned);
+            return response;
+        }).collect(Collectors.toList());
+    }
+
 }
